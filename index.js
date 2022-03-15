209f94f7fa8a9bc915dc413f4ca8b60f060945c7
Filip <filipdopa@yahoo.com>
2022-03-15 14:44:52 +0100

    Added event listener instead of onclick

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+document.querySelector(".generate-password").addEventListener('click', getPasswords);
 
 function genRandomPassword() {
     const chars = [1,2,3,4,5,6,7,8,"q","w","e","r","t","y","u","i","o","p","[","]","a","s","d","f","g","h","j","k","l",";","'","z","x","c","v","b","n","m",",",".","/","Q","W","E","R","T","Y","U","I","O","P","{","}","A","S","D","F","G","H","J","K","L",":","|","Z","X","C","V","B","N","M","<",">","?","!","@","#","$","%","^","&","*","(",")","_","+"];
@@ -15,10 +16,7 @@ function genRandomPassword() {
 function getPasswords() {
     let passwords = document.querySelectorAll(".passwords");
 
-    for (let j = 0; j < passwords.length; j++) {
-    passwords[j].textContent = genRandomPassword();
+    for (let i = 0; i < passwords.length; i++) {
+    passwords[i].textContent = genRandomPassword();
     }
 }
-
-
-
